9b20b1eb3fe5e31d9d95126643592fe0653386bb
LAPTOP-Q33DMCK8\HP <kaltay13@ku.edu.tr>
2018-06-20 12:29:13 -0700

    add main

---
+++ MatterMark/main.py
@@ -0,0 +1,7 @@
+from MatterMark import send_mail as sender
+
+# #######Get mattermark digest
+
+
+# #######Invoke send mail
+sender.send_mail()
--- MatterMark/send_mail.py
+++ MatterMark/send_mail.py
@@ -3,25 +3,26 @@ import smtplib
 from email.mime.text import MIMEText
 # from MatterMark import dd_utils
 
-# Access google account
-address = os.environ.get("mail_addr")
-password = os.environ.get("mail_pass")
-"""
-account = dd_utils.get_mail_credentials()
-address = dd_utils.get_mail_address(account)
-password = dd_utils.get_mail_pass(account)
-"""
 
-send_file = open("query_results.txt", "r")
-msg = MIMEText(send_file.read())
-send_file.close()
+def send_mail():
+    # Access google account
+    address = os.environ.get("mail_addr")
+    password = os.environ.get("mail_pass")
+    """
+    account = dd_utils.get_mail_credentials()
+    address = dd_utils.get_mail_address(account)
+    password = dd_utils.get_mail_pass(account)
+    """
 
-msg['Subject'] = "Daily mattermark"
-msg['To'] = "kerem.alty@gmail.com"
-msg['From'] = address
+    send_file = open("query_results.txt", "r")
+    msg = MIMEText(send_file.read())
+    send_file.close()
 
-print(msg)
-s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-s.login(address, password)
-s.sendmail(msg['From'], msg['To'], msg.as_string())
-s.quit()
+    msg['Subject'] = "Daily mattermark"
+    msg['To'] = "kerem.alty@gmail.com"
+    msg['From'] = address
+
+    s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    s.login(address, password)
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+    s.quit()
