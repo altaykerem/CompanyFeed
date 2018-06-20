6a7ab1b966de60b1fc17ce263ba720a4abc6f9e3
LAPTOP-Q33DMCK8\HP <kaltay13@ku.edu.tr>
2018-06-20 12:10:03 -0700

    heroku env get mail

--- MatterMark/send_mail.py
+++ MatterMark/send_mail.py
@@ -1,11 +1,16 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
-from MatterMark import dd_utils
+# from MatterMark import dd_utils
 
-# google account
+# Access google account
+address = os.environ.get("mail_addr")
+password = os.environ.get("mail_pass")
+"""
 account = dd_utils.get_mail_credentials()
 address = dd_utils.get_mail_address(account)
 password = dd_utils.get_mail_pass(account)
+"""
 
 send_file = open("query_results.txt", "r")
 msg = MIMEText(send_file.read())
