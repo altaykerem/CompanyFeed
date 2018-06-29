f76c53c274a0b62255cca1f3c273f7c263c884a1
LAPTOP-Q33DMCK8\HP <kaltay13@ku.edu.tr>
2018-06-28 17:18:37 -0700

    no message

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ f.close()
 # write the result in the file query_results.txt
 rising_query.EmergingQuery().write_query()
 angel_query.AssistanceQuery().write_query()
-latest_query.LatestQuery().write_query()
+# latest_query.LatestQuery().write_query()
 
 # #######Invoke send mail
 sender.send_mail()
